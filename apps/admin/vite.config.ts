0c0adbd8114f18efbe4e8a47f9232fff0bfb7c10
liyang <2008042226@163.com>
2026-09-11 09:11:14 +0800

    ci(github workflows): 优化ci流程

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -4,27 +4,32 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE_PATH,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  plugins: [
-    vue(),
-    Components(),
-    tailwindcss(),
-    vueDevTools(),
-    Components({
-      resolvers: [
-        AntDesignVueResolver({
-          importStyle: false,
-        }),
-      ],
-    }),
-  ],
+    plugins: [
+      vue(),
+      Components(),
+      tailwindcss(),
+      vueDevTools(),
+      Components({
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: false,
+          }),
+        ],
+      }),
+    ],
+  };
 });
