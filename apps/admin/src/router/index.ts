6e8a9c3c826b0d68ea6ca6e43ee95597d1ae0558
liyang <2008042226@163.com>
2026-09-14 14:56:18 +0800

    build(admin): 切换vue路由模式为hash模式

--- apps/admin/src/router/index.ts
+++ apps/admin/src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import { useProgressStore } from '@/stores/progress';
 import { useRouteStore } from '@/stores/route';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
